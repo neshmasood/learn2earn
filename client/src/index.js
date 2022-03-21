df8d4af93f96f01231cb54809fa4e38365a3f868
neshmasood <neshtam@gmail.com>
2022-03-21 12:25:43 -0500

    modifield app.js and index.html in public folder

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter} from "react-router-dom"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 ReactDOM.render(
+  <BrowserRouter>
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
